e617589f083c97acc784d897d4970af59509c553
kjmin622 <kjmin622@naver.com>
2020-03-15 02:15:17 +0900

    mobile

--- js/game.js
+++ js/game.js
@@ -1,4 +1,10 @@
 window.onload=function(){
+    document.documentElement.addEventListener('touchstart', function (event){
+        if(event.touches.length>1){
+            event.preventDefault();
+        }
+    }, false); // 모바일 더블터치 확대 방지
+
     var height = 0;
     function char_struct(){
         // face 300px

7aaa1c23706f2b9c80fb44c314f112cef5970543
kjmin622 <kjmin622@naver.com>
2020-03-15 02:54:04 +0900

    anni

--- js/game.js
+++ js/game.js
@@ -1,17 +1,28 @@
 window.onload=function(){
-    document.documentElement.addEventListener('touchstart', function (event){
-        if(event.touches.length>1){
-            event.preventDefault();
-        }
-    }, false); // 모바일 더블터치 확대 방지
-
+    var play=true;
     var height = 0;
+    char_struct();
     function char_struct(){
-        // face 300px
         document.getElementById("face").style.top=(300-height)+"px";
         document.getElementById("lace").style.top=(300-height)+"px";
         document.getElementById("leg").style.top=(300-height)+"px";
         
     }
-    char_struct();
+
+    var lace = 1;
+    var lace_check = true;
+    var leg = 1;
+    setInterval( // char animation
+        function(){
+            if(play){
+                if(lace_check)lace+=1;
+                else lace-=1;
+                leg+=1;
+                if(lace<1){lace=1; lace_check=true;}
+                if(lace>3){lace=2; lace_check=false;}
+                if(leg>2) leg=1;
+                document.getElementById("leg").setAttribute('src','src/image/char/'+leg+'_leg.png');
+                document.getElementById("lace").setAttribute('src','src/image/char/'+lace+'_lace.png');
+            }
+        }, 120);
 }

09102bb69573dc975d2b3d29ad30af3c9af08948
kjmin622 <kjmin622@naver.com>
2020-03-15 17:01:19 +0900

    200315

--- js/game.js
+++ js/game.js
@@ -1,5 +1,20 @@
 window.onload=function(){
-    var play=true;
+    var play=false;
+
+    //////////////////menu//////////////////////////////////////
+    document.getElementById('Pbutton').onclick = function(){
+        if(play==false){
+            play=true;
+            document.getElementById("Pbutton").setAttribute('src','src/image/menu/button'+2+'.png');
+        }
+        else{
+            play=false;
+            document.getElementById("Pbutton").setAttribute('src','src/image/menu/button'+1+'.png');
+        }
+    }
+
+
+    //////////////////background move////////////////////////////
     function move_cinema(){
         var left=0;
         var move=setInterval(function(){
@@ -13,6 +28,7 @@ window.onload=function(){
     move_cinema();
 
 
+    //////////////////char struct////////////////////////////
     var height = 0;
     char_struct();
     function char_struct(){
@@ -22,6 +38,8 @@ window.onload=function(){
         
     }
 
+
+    //////////////////char moving////////////////////////////
     var lace = 1;
     var lace_check = true;
     var leg = 1;
@@ -39,38 +57,40 @@ window.onload=function(){
             }
         }, 120);
     
+    //////////////////jump////////////////////////////
     var jump=0;
     document.getElementById('screen').onclick = function(){
-        if(jump==0){
-            document.getElementById("jump1").style.visibility="visible";
-            setTimeout(function(){
-                document.getElementById("jump1").style.visibility="hidden";
-            },100);
-        }
-        if(jump==1){
-            document.getElementById("jump2").style.top=300-height+"px";
-            document.getElementById("jump2").style.visibility="visible";
-            setTimeout(function(){
-                document.getElementById("jump2").style.visibility="hidden";
-            },100);
-        }
-        if(jump!=2){
-            jump+=1;
-            var up_speed=jump==1 ? 23:17;
-            var up = setInterval(
-                function(){
-                    height+=up_speed;
-                    up_speed-=1;
-                    char_struct();
-                },20
-            );
-            setTimeout(function() {
-                clearTimeout(up);
-                down_jump();
-            },200);
+            if(play){
+            if(jump==0){
+                document.getElementById("jump1").style.visibility="visible";
+                setTimeout(function(){
+                    document.getElementById("jump1").style.visibility="hidden";
+                },100);
+            }
+            if(jump==1){
+                document.getElementById("jump2").style.top=300-height+"px";
+                document.getElementById("jump2").style.visibility="visible";
+                setTimeout(function(){
+                    document.getElementById("jump2").style.visibility="hidden";
+                },100);
+            }
+            if(jump!=2){
+                jump+=1;
+                var up_speed=jump==1 ? 23:17;
+                var up = setInterval(
+                    function(){
+                        height+=up_speed;
+                        up_speed-=1;
+                        char_struct();
+                    },20
+                );
+                setTimeout(function() {
+                    clearTimeout(up);
+                    down_jump();
+                },200);
+            }
         }
     }
-
     function down_jump(){
         var down_speed=-4;
         var down = setInterval(
